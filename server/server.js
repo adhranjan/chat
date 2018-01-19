98e0b0adc33a5d1d645b727ef63ce095f0d12655
Ranjan Adhikari <adh.ranjan@gmail.com>
2018-01-19 23:05:27 +0530

    chat broadcast

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
+
+const {generateMessage} = require('./util/message')
 const publicPath = path.join(__dirname,'../public')
 const port = process.env.PORT || 3000
 var app = express()
@@ -12,6 +14,11 @@ var io = socketIO(server)
 
 io.on('connection',(socket)=>{
   // console.log("new user")
+
+  socket.emit('newMessage',generateMessage('Admin','Welcome to chat'))
+  socket.broadcast.emit('newMessage',generateMessage('Admin','New User Joint'))
+
+
   socket.on('createMessage',(message)=>{ // event when i get message
       io.emit('newMessage',{ // broad cast the message
         from:message.from,
